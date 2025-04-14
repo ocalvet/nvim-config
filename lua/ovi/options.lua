81636035182d5aa38a17f8bd2f4d897052fcc140
Ovidio R. Calvet <ovidio_calvet@ncci.com>
2025-04-14 19:08:01 -0400

    Show relative linenumbers in netrw

--- lua/ovi/options.lua
+++ lua/ovi/options.lua
@@ -19,3 +19,4 @@ vim.o.termguicolors = true -- Enable 24-bit RGB colors
 -- netrw settings
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
+vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
